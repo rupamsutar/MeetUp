65447834be65fa5b65a12a8c2edbe4a0bfdb505a
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-24 13:59:36 +0530

    outputting a list of meetups

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg',
+        address: "Nehru Maidan Vambori, Rahuri 10",
+        description: 'A first meetup description'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg',
+        address: "Nehru Maidan Vambori, Rahuri 20",
+        description: 'A Second meetup description'
+    }
+];
+
+function Homepage() {
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default Homepage;
