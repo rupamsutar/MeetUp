6a3e12a4a2157f9bef673c66fdc66f8dbf844ffb
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-24 19:10:46 +0530

    Preparion paths with get static paths and working with fall back content

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,38 @@ function MeetupDetails() {
     )
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: "m1"
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2'
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+
+    const meetupId = context.params.meetupId;
+
+    return {
+        props: {
+            meetupData: {
+                img: "https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg",
+                title: 'A First Meetup',
+                address: 'Nehru Maidan Vambori',
+                description: 'A First Meetup'
+            }
+        }
+    }
+}
+
 export default MeetupDetails;
