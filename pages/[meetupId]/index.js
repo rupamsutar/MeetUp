32b2844459812f1406a36a964563e20e2610ff9d
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-24 15:41:41 +0530

    Adding Custom Components and css modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail 
+            img="https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg"
+            title='A First Meetup'
+            address='Nehru Maidan Vambori'
+            description='A first meetup'
+        />
+    )
+}
+
+export default MeetupDetails;
