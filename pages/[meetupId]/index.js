6adb6f85c77b8702208289625fa7aba0b86b8fcb
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-24 19:36:01 +0530

    Introducing to API Routes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -4,7 +4,7 @@ import MeetupDetail from "../../components/meetups/MeetupDetail";
 function MeetupDetails() {
     return (
         <MeetupDetail 
-            img="https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg"
+            image="https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg"
             title='A First Meetup'
             address='Nehru Maidan Vambori'
             description='A first meetup'
@@ -37,7 +37,7 @@ export async function getStaticProps(context) {
     return {
         props: {
             meetupData: {
-                img: "https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg",
+                image: "https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg",
                 title: 'A First Meetup',
                 address: 'Nehru Maidan Vambori',
                 description: 'A First Meetup'
